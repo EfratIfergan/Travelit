55d1a5de5963c7388d7f92c77f66efd3f16b780c
EfratIfergan <efrat3888@gmail.com>
2023-02-02 13:26:39 +0200

    Add Navigation Feature

---
+++ Travelit-FE/src/shared/Navigation/MainNavigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import MainHeader from "./MainHeader";
+import './MainNavigation.css';
+
+const MainNavigation = props => {
+    return <MainHeader>
+
+    </MainHeader>
+}
+
+export default MainNavigation;
--- Travelit-FE/src/user/components/UsersList.js
+++ Travelit-FE/src/user/components/UsersList.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import UserItem from './UserItem';
+import Card from '../../shared/UIElement/Card'
 import './UsersList.css';
 
 const UsersList = props => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
