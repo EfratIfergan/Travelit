4322981e49013ec2acce2ae8b4514020c8cf329d
EfratIfergan <efrat3888@gmail.com>
2023-02-13 00:16:43 +0200

    Added routes to user places

--- Travelit-FE/src/App.js
+++ Travelit-FE/src/App.js
@@ -8,6 +8,7 @@ import {
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/Navigation/MainNavigation';
 
 const App = () => {
@@ -19,6 +20,9 @@ const App = () => {
         <Route path="/" exact>
           <Users />
         </Route>
+        <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
         <Route path="/places/new" exact>
           <NewPlace />
         </Route>
